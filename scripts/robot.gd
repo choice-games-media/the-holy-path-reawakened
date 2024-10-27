6eb1ed24fc662bd1dfa1720f867289a38afaef65
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-10-27 17:56:23 +1100

    Enemy!!
    Also renamed "enemy" to "robot" and reformatted scripts

---
+++ scripts/robot.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const SPEED = 50
+var direction = 1  # Positive values = right, negative values = left
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		direction = 1
+		sprite.flip_h = false
+	if ray_cast_right.is_colliding():
+		direction = -1
+		sprite.flip_h = true
+
+	position.x += direction * SPEED * delta

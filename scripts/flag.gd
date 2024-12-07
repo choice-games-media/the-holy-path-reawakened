2215c7bdf72bbe19e387249c1d016477b8da48c4
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-12-07 13:50:48 +1100

    Annoyances

--- scripts/flag.gd
+++ scripts/flag.gd
@@ -7,6 +7,6 @@ var victory_label: Label = get_tree().get_current_scene().get_node("HUD/MarginCo
 
 func _on_body_entered(body: Node2D) -> void:
 	victory_label.show()
-	body.get_node("Camera2D").reparent(self)
+	# body.get_node("Camera2D").reparent(self)
 	body.frozen = true
 	win_sound.play()

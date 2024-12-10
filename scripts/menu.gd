bc618ebea19708b4325428bec30d3fbf63c21356
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-12-10 15:26:27 +1100

    Theme fuckery + foundations of an options menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,12 +2,21 @@ extends Control
 
 
 func _ready() -> void:
+	DisplayServer.window_set_min_size(Vector2i(1152, 648))
 	MusicPlayer.update_music("Menu")
 
 
 func _on_play_button_pressed() -> void:
 	MusicPlayer.update_music("Game")
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
 func _on_exit_button_pressed() -> void:

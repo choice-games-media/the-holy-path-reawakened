be22c0c13c4d0fd7459dea024d031af501a5dca8
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-12-11 13:48:17 +1100

    Functional options menu

--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -2,16 +2,19 @@ extends AudioStreamPlayer2D
 
 const MENU_MUSIC = preload("res://assets/music/Kevin MacLeod - Galactic Rap.mp3")
 const GAME_MUSIC = preload("res://assets/music/Elektronomia - Sky High.mp3")
+var scene_playing_music: String
 
 
 func update_music(scene_name: String):
-	self.stop()
-	match scene_name:
-		"Menu":
-			self.set_stream(MENU_MUSIC)
-			self.set_volume_db(0)
+	if scene_playing_music != scene_name:
+		scene_playing_music = scene_name
+		self.stop()
+		match scene_name:
+			"Menu":
+				self.set_stream(MENU_MUSIC)
+				self.set_volume_db(0)
 
-		"Game":
-			self.set_stream(GAME_MUSIC)
-			self.set_volume_db(-15)
-	self.play()
+			"Game":
+				self.set_stream(GAME_MUSIC)
+				self.set_volume_db(-15)
+		self.play()
--- scripts/options.gd
+++ scripts/options.gd
@@ -1,5 +1,35 @@
 extends Control
 
+@onready var music_spin_box: SpinBox = $MarginContainer/VBoxContainer/GridContainer/MusicSpinBox
+@onready var music_slider: Slider = $MarginContainer/VBoxContainer/GridContainer/MusicSlider
+@onready var sfx_spin_box: SpinBox = $MarginContainer/VBoxContainer/GridContainer/SfxSpinBox
+@onready var sfx_slider: Slider = $MarginContainer/VBoxContainer/GridContainer/SfxSlider
+
+
+func _on_music_spin_box_value_changed(value: float) -> void:
+	music_slider.set_value(value)
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	music_spin_box.set_value(value)
+	AudioServer.set_bus_volume_db(1, value)
+
 
 func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_sfx_spin_box_value_changed(value: float) -> void:
+	sfx_slider.set_value(value)
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	sfx_spin_box.set_value(value)
+	AudioServer.set_bus_volume_db(2, value)
+
+
+func _on_full_screen_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
